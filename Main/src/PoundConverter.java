a5e96953f2f9f8834b953b8da5f02a78ee6b77d6
Andrew G <58037516+Vardantex@users.noreply.github.com>
2021-04-18 19:35:07 -0400

    Kilometer converter
    This program converts pounds into kilometers.

---
+++ Main/src/PoundConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PoundConverter {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int Pound = 1;
+        double Kilo = 0.45359237d;
+        double custompKilo;
+        double onepKilo = Pound * Kilo;
+
+        System.out.println("One pound converted to a kilo is : " + onepKilo );
+        System.out.println();
+        System.out.println("Enter a number of pounds to convert it to kilo: ");
+        custompKilo = keyboard.nextDouble();
+
+        //Create the converted method
+        double convertedpKilo = custompKilo * Kilo;
+
+        System.out.println("Converted value is: " + convertedpKilo);
+
+    }
+}
