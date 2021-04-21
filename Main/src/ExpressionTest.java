41dc8eb049300ce16d08ee1af7f82781794e57c8
Andrew G <58037516+Vardantex@users.noreply.github.com>
2021-04-21 12:07:48 -0400

    Conditional Expression

---
+++ Main/src/ExpressionTest.java
@@ -0,0 +1,34 @@
+public class ExpressionTest {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        //Call the calculate mehtod and add values
+        calculateScore(true, 800, levelCompleted, bonus);
+
+        calculateScore(true, 10_000, 8, 200);
+
+
+    }
+        public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+            if (gameOver) {
+                int finalScore = score + (levelCompleted * bonus);
+                finalScore += 2000;
+                System.out.println("FInal score: " + finalScore);
+            }
+
+        
+
+        }
+
+
+
+        }
+
+
+
+
---
+++ Main/src/ExpressionTest2.java
@@ -0,0 +1,34 @@
+public class ExpressionTest2 {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        //Call the calculate mehtod and add values
+        calculateScore(true, 800, levelCompleted, bonus);
+
+        calculateScore(true, 10_000, 8, 200);
+
+
+    }
+
+    //Change method to return a finalScore integer
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("FInal score: " + finalScore);
+            return finalScore;
+        } else {
+        return -1;
+        }
+
+    }
+}
+
+
+
+
