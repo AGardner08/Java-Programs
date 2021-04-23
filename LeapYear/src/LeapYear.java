5f619d9e33ee867ade0072736c2a8c76b0c9d486
Andrew G <58037516+Vardantex@users.noreply.github.com>
2021-04-23 13:28:53 -0400

    LeapYear v3

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -2,16 +2,22 @@ public class LeapYear {
 
     public static boolean isLeapYear(int year) {
 
-        if(year >=1 && year < 9999) {
+        if(year >=1 && year < 9999) {//outer if else
 
             if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-                System.out.println("Leap year");
+                System.out.println(year + " is Leap year");
+                return true;
+            } else if (year % 100 == 0 && year % 400 == 0){
+                System.out.println(year + " is Leap year? (true)");
                 return true;
             } else {
-                System.out.println("Not a leap year ");
+                System.out.println(year + " is False");
                 return false;
+
             }
+
         } else { //outer if else
+            System.out.println(year + " is False");
             return false;
         }
 
--- LeapYear/src/main.java
+++ LeapYear/src/main.java
@@ -3,15 +3,22 @@ public class main {
     public static void main(String[] args) {
 
 
+        //Must be false
         LeapYear.isLeapYear(1700);
         LeapYear.isLeapYear(1800);
         LeapYear.isLeapYear(1900);
         LeapYear.isLeapYear(2100);
+        LeapYear.isLeapYear(2200);
+        LeapYear.isLeapYear(2300);
+        LeapYear.isLeapYear(2500);
+        LeapYear.isLeapYear(2600);
 
 
+        //Must be true
         LeapYear.isLeapYear(1600);
         LeapYear.isLeapYear(2000);
         LeapYear.isLeapYear(2400);
+        //LeapYear.isLeapYear(1924);
 
     }
 }
